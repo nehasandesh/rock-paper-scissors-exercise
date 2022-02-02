50bdccd53f7b7c820b33073431593fe250e14c62
nehasandesh <ns1193@georgetown.edu>
2022-02-02 02:24:40 -0500

    asked for a user input

--- game.py
+++ game.py
@@ -5,3 +5,25 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+
+#ask for a user input
+
+u = input("Please choose one: 'Rock', 'Paper', 'Scissors': ")
+
+print("User chose:", u)
+#validations
+
+
+
+#computer choice
+
+
+
+#determine winner
+
+
+
+#final results
